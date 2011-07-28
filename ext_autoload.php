9962d5d1cc53e9f2c08d9c25378fb9839caf5bc0
speedprogs_de <speedprogs_de@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-28 21:05:33 +0000

    added missing view helpers to ext_autoload.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sp_guestbook/trunk@50259 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -2,11 +2,16 @@
 	$extensionClassesPath = t3lib_extMgm::extPath('sp_guestbook', 'Classes/');
 
 	return array(
-		'tx_spguestbook_controller_entrycontroller'        => $extensionClassesPath . 'Controller/EntryController.php',
-		'tx_spguestbook_domain_model_entry'                => $extensionClassesPath . 'Domain/Model/Entry.php',
-		'tx_spguestbook_domain_repository_entryrepository' => $extensionClassesPath . 'Domain/Repository/EntryRepository.php',
-		'tx_spguestbook_service_email'                     => $extensionClassesPath . 'Service/Email.php',
-		'tx_spguestbook_service_security'                  => $extensionClassesPath . 'Service/Security.php',
-		'tx_spguestbook_utility_typoScript'                => $extensionClassesPath . 'Utility/TypoScript.php',
+		'tx_spguestbook_controller_entrycontroller'            => $extensionClassesPath . 'Controller/EntryController.php',
+		'tx_spguestbook_domain_model_entry'                    => $extensionClassesPath . 'Domain/Model/Entry.php',
+		'tx_spguestbook_domain_repository_entryrepository'     => $extensionClassesPath . 'Domain/Repository/EntryRepository.php',
+		'tx_spguestbook_service_email'                         => $extensionClassesPath . 'Service/Email.php',
+		'tx_spguestbook_service_security'                      => $extensionClassesPath . 'Service/Security.php',
+		'tx_spguestbook_utility_typoScript'                    => $extensionClassesPath . 'Utility/TypoScript.php',
+		'tx_spguestbook_viewhelpers_abstractcaptchaviewhelper' => $extensionClassesPath . 'ViewHelpers/AbstractCaptchaViewHelper.php',
+		'tx_spguestbook_viewhelpers_captchaviewhelper'         => $extensionClassesPath . 'ViewHelpers/CaptchaViewHelper.php',
+		'tx_spguestbook_viewhelpers_freecapviewhelper'         => $extensionClassesPath . 'ViewHelpers/FreeCapViewHelper.php',
+		'tx_spguestbook_viewhelpers_mathguardviewhelper'       => $extensionClassesPath . 'ViewHelpers/MathGuardViewHelper.php',
+		'tx_spguestbook_viewhelpers_recaptchaviewhelper'       => $extensionClassesPath . 'ViewHelpers/RecaptchaViewHelper.php',
 	);
 ?>
